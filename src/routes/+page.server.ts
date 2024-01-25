bc9f100294128b269f594812c6d8faf9d4e37b71
novaotp <rahmansajidur30@gmail.com>
2024-01-25 16:33:39 +0100

    feat: added dummy data to work without db, added text for when array is empty

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,13 +2,39 @@ import type { Post, RawPost } from '../models/Post';
 import type { User } from '../models/User';
 import type { PageServerLoad } from './$types';
 
+// When you don't have access to the database
+/* const getUser = (): User => {
+	return {
+		id: 1,
+		nickname: "novaotp",
+		is_private: false,
+		first_name: "Sajidur",
+		last_name: "Rahman",
+		email: "rahmansajidur30@gmail.com",
+		created_at: new Date(),
+		updated_at: new Date()
+	}
+}
+
+const getPosts = (): Post[] => {
+	return [
+		{
+			id: 1,
+			creator: getUser(),
+			description: "Look at my new image",
+			image: "CampusEPTM_WallPaper21c_JPG100.jpeg",
+			created_at: new Date()
+		}
+	]
+} */
+
 export const load: PageServerLoad = async () => {
-    const postResponse = await fetch("http://127.0.0.1:4000/api/posts", { method: "GET" });
+    const postResponse = await fetch("http://localhost:4000/api/posts", { method: "GET" });
     const rawPosts: RawPost[] = await postResponse.json();
 
 	const posts: Post[] = [];
 	for (const rawPost of rawPosts) {
-		const userResponse = await fetch(`http://127.0.0.1:4000/api/user/${rawPost.creator_id}`, { method: "GET" });
+		const userResponse = await fetch(`http://localhost:4000/api/user/${rawPost.creator_id}`, { method: "GET" });
 		const user: User = await userResponse.json();
 
 		const post: Post = {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-	import Post from "$lib/Post.svelte";
-    import type { PageData } from './$types';
-	
+	import Post from '$lib/Post.svelte';
+	import type { PageData } from './$types';
+
 	export let data: PageData;
 </script>
+
 <h1>Welcome to Amstramgram</h1>
 
-{#each data.posts as post}
-<Post data={post} />
-{/each}
+{#if data.posts.length > 0}
+	{#each data.posts as post}
+		<Post data={post} />
+	{/each}
+{:else}
+	<p>No post has been created yet...</p>
+{/if}
