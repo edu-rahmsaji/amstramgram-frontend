d1108d3f8f4e16d2949962951e81c59db8cdaf6b
novaotp <rahmansajidur30@gmail.com>
2024-01-25 16:32:33 +0100

    feat: added a navbar in layout

---
+++ src/lib/Navbar/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Link from './Link.svelte';
+	import {
+		IconHome as Home,
+        IconCompass as Explore,
+        IconCirclePlus as AddPost,
+        IconHelp as Help,
+		IconUserCircle as UserProfile
+	} from '@tabler/icons-svelte';
+</script>
+
+<!--
+@component
+
+Renders a navbar adapted for devices under 1024px wide.
+-->
+
+<nav
+	class="lg:hidden relative w-full h-20 shadow-[0px_-4px_3px_rgba(50,50,50,0.25)] dark:shadow-[0px_-4px_3px_rgba(10,10,10,0.25)]"
+>
+	<ul class="relative w-full h-full py-5 flex justify-evenly items-center">
+		<Link href="/" label="Home" icon={Home} />
+		<Link href="/explore" label="Explore" icon={Explore} />
+		<Link href="/post/add" label="Add a post" icon={AddPost} />
+        <Link href="/help" label="Help / Support" icon={Help} />
+		<Link href="/user" label="Contact" icon={UserProfile} />
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+    import Navbar from "$lib/Navbar/Navbar.svelte";
     import "../app.css";
 </script>
 
-<slot />
+<div class="relative w-full h-full flex flex-col">
+    <main class="relative w-full flex-grow flex flex-col">
+        <slot />
+    </main>
+    <Navbar />
+</div>
